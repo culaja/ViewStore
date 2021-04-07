cbb0ed715f6ed4cd1dd81da989a98937a0903e65
Stanko Culaja <stanko.culaja@kolotree.com>
2021-04-07 15:18:13 +0200

    Finished sample.

---
+++ Samples/QueueingSystemEventProcessor/CustomerDequeued.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace QueueingSystemEventProcessor
+{
+    public sealed class CustomerDequeued : IEvent
+    {
+        public string QueueName { get; }
+        public int TicketNumber { get; }
+        public DateTime DequeuedTime { get; }
+        public string ViewId => "SingleView";
+
+        public CustomerDequeued(
+            string queueName,
+            int ticketNumber,
+            DateTime dequeuedTime)
+        {
+            QueueName = queueName;
+            TicketNumber = ticketNumber;
+            DequeuedTime = dequeuedTime;
+        }
+
+        public override string ToString()
+        {
+            return $"[{nameof(CustomerDequeued)}] {nameof(QueueName)}: {QueueName}, {nameof(TicketNumber)}: {TicketNumber}, {nameof(DequeuedTime)}: {DequeuedTime}";
+        }
+    }
+}
---
+++ Samples/QueueingSystemEventProcessor/CustomerServed.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace QueueingSystemEventProcessor
+{
+    public sealed class CustomerServed : IEvent
+    {
+        public string QueueName { get; }
+        public int TicketNumber { get; }
+        public string CounterName { get; }
+        public DateTime ServedTime { get; }
+        
+        public string ViewId => "SingleView";
+
+        public CustomerServed(
+            string queueName,
+            int ticketNumber,
+            string counterName,
+            DateTime servedTime)
+        {
+            QueueName = queueName;
+            TicketNumber = ticketNumber;
+            CounterName = counterName;
+            ServedTime = servedTime;
+        }
+
+        public override string ToString()
+        {
+            return $"[{nameof(CustomerServed)}] {nameof(QueueName)}: {QueueName}, {nameof(TicketNumber)}: {TicketNumber}, {nameof(CounterName)}: {CounterName}, {nameof(ServedTime)}: {ServedTime}";
+        }
+    }
+}
