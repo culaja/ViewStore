12a97e64eb35aacf4b817c8f7c000811e99802a5
Stanko Culaja <stanko.culaja@kolotree.com>
2021-11-11 18:45:36 +0100

    Fixed bug in cache when draining cache.

--- Cache/ViewStore.Cache/ViewMetaData.cs
+++ Cache/ViewStore.Cache/ViewMetaData.cs
@@ -7,10 +7,6 @@ namespace ViewStore.Cache
         public const string MetaDataId = "28963e0b-b51a-459b-be9d-c5ea1aa6f843";
 
         public static ViewEnvelope MetaDataEnvelopeFor(GlobalVersion globalVersion) =>
-            new(
-                "28963e0b-b51a-459b-be9d-c5ea1aa6f843", 
-                null!,
-                globalVersion,
-                MetaData.New());
+            ViewEnvelope.EmptyWith("28963e0b-b51a-459b-be9d-c5ea1aa6f843", globalVersion);
     }
 }
